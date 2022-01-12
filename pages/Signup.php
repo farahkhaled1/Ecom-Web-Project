868a814c61a868c89c59a810cc1c447b5f9ded13
farahkhaled1 <farah1900784@miuegypt.edu.eg>
2022-01-13 00:47:17 +0200

    added footer

--- pages/Signup.php
+++ pages/Signup.php
@@ -1,41 +1,41 @@
 <?php
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
-$username = $email =$password =  "";
-$username_err = $email_err =$password_err =  "";
- 
+$username = $email = $password =  "";
+$username_err = $email_err = $password_err =  "";
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Validate username
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
         $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
+    } else {
         // Prepare a select statement
         $sql = "SELECT id FROM userr WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
+
             // Set parameters
             $param_username = trim($_POST["username"]);
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 /* store result */
                 mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
+
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     $username_err = "This username is already taken.";
-                } else{
+                } else {
                     $username = trim($_POST["username"]);
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -45,42 +45,42 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     //Validate email
-    if(empty(trim($_POST["email"]))){
-      $email_err = "Please enter an email";     
-     } else{
-      $email = trim($_POST["email"]);
-  }
-    
+    if (empty(trim($_POST["email"]))) {
+        $email_err = "Please enter an email";
+    } else {
+        $email = trim($_POST["email"]);
+    }
+
     // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
         $password_err = "Password must have atleast 6 characters.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
-    
+
+
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($email_err) && empty($password_err)){
-        
+    if (empty($username_err) && empty($email_err) && empty($password_err)) {
+
         // Prepare an insert statement
         $sql = "INSERT INTO userr (username,email,password) VALUES (?,?,?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_email ,$param_password);
-            
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_email, $param_password);
+
             // Set parameters
             $param_username = $username;
             $param_email = $email;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Redirect to login page
                 header("welcome.php");
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -88,26 +88,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Close connection
     mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Sign up</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../styles/StyleLogin&Signup.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
+
 <body>
-<div id="navbar">
-    <?php 
-    include "../component/navbar.html"
-    ?>
-</div>
+    <div id="navbar">
+        <?php
+        include "navbar.html"
+        ?>
+    </div>
 
 
     <div class="wrapper">
@@ -118,12 +122,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Username</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group">
                 <label>email</label>
                 <input type="email" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
                 <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>  
+            </div>
             <div class="form-group">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
@@ -135,6 +139,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <p>Already have an account? <a href="login.php" class="redirect">Login here</a>.</p>
         </form>
-    </div>    
+    </div>
+
+    <div id="footer">
+        <?php
+        include "footer.html"
+        ?>
+    </div>
 </body>
+
 </html>
---
+++ pages/home.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/Style.css">
+    <title>Glow Glam | Ecommerce Website</title>
+</head>
+
+<body>
+
+    <body>
+
+
+
+        </div>
+        <div class="header">
+
+            <div class="container">
+                <div class="navbar">
+                    <div class="logo">
+                        <a href="index.html"><img src="../images/logo.png" width="125px"></a>
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li> <a class="nav-items" href=""> Home </a> </li>
+                            <li> <a class="nav-items" href=""> Products </a> </li>
+                            <li> <a class="nav-items" href=""> About </a> </li>
+                            <li> <a class="nav-items" href="http://localhost/Web-project/pages/contact.php"> Contact </a> </li>
+                            <li> <a class="nav-items" href="http://localhost/Web-project/pages/login.php"> Log in </a> </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <h1> The Beauty Essentials <br> You Need </h1>
+                    <p> Show up and steal the show <br> with an extra boost of confidence. </p>
+                    <a href="" class="btn">Shop Now! </a>
+                </div>
+                <div class="col-2 img">
+                    <img src="../images/pic9.png">
+                </div>
+            </div>
+
+            <div id="cards">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://images.unsplash.com/photo-1594125311687-3b1b3eafa9f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Perfumes</h5>
+                        <p class="card-text">A fragrance to match your mood.</p>
+                        <a href="#" class="btn">Browse Our Perfumes</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="https://images.unsplash.com/photo-1596462502278-27bfdc403348?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Cosmetics</h5>
+                        <p class="card-text">Turn into a glamorous you.</p>
+                        <a href="#" class="btn">Browse Cosmetics</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="https://images.unsplash.com/photo-1607779097040-26e80aa78e66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Manicures</h5>
+                        <p class="card-text">Your manicure speaks to the world for you.</p>
+                        <a href="#" class="btn">Browse Manicures</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div id="footer">
+                <?php
+                include "footer.html"
+                ?>
+            </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+
+</html>
